cb0d2784f88de857b10278ad7aff5dc7366fb265
Tabish <tabish@vgenomics>
2025-09-05 18:20:01 +0530

    Rare Predict

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
 import ChatBot from '@/components/ChatBot/index';
-
+import { Suspense } from "react";
 export default function Result() {
 
     return (
-        <ChatBot />
+        <Suspense fallback={<div>Loading...</div>}>
+            <ChatBot />
+        </Suspense>
     );
 }
